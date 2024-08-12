4687447a74c705ead400deddf3e0c39f4146bd48
henry <crofthans@yahoo.com.au>
2024-08-12 21:15:23 +1000

    Week 3 Tasks: moved adhoc function from list.c to main

--- Week 3/list.c
+++ Week 3/list.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
+#include <time.h>
 
 
 // Function to create and return a new empty list.
--- Week 3/main.c
+++ Week 3/main.c
@@ -1,5 +1,104 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "list.h"
+#include <time.h>
+
+
+
+//Practical Analysis: Step 3
+long f1(long n) {
+    long k = 0;
+    for (long i = 0; i < n; i++) {
+        for (long j = 0; j < n; j++) {
+            k++;
+        }
+    }
+    return k;
+}
+
+void f2(long n) {
+    long k = 0;
+    for (long i = 0; i < n; i++) {
+        for (long j = 0; j < i; j++) {
+            k++;
+        }
+        for (long t = 0; t < 10000; t++) {
+            k++;
+        }
+    }
+}
+
+void f3(long n) {
+    if (n > 0) {
+        f3(n / 2);
+        f3(n / 2);
+    }
+}
+
+void f4(long n) {
+    if (n > 0) {
+        f4(n / 2);
+        f4(n / 2);
+        f2(n);
+    }
+}
+
+void f5(long n) {
+    long k = 0;
+    for (long i = 0; i < 10; i++) {
+        long j = n;
+        while (j > 0) {
+            f1(1000);
+            k++;
+            j = j / 2;
+        }
+    }
+}
+
+void f6(long n) {
+    f2(n);
+    f3(n);
+    f5(n);
+}
+
+void f7(long n) {
+    long k = 0;
+    for (long i = 0; i < f1(n); i++) {
+        k++;
+    }
+    for (long j = 0; j < n; j++) {
+        k++;
+    }
+}
+
+// Moved this from list.c
+void list_adhoc_test(List* self) {
+	List newList = new_list();
+	int quit = 0;
+
+	while (!quit) {
+		int value;
+		printf("\nMenu\n");
+		printf("0. Exit\n");
+		printf("1. f1\n");
+		printf("2. f2\n");
+		printf("3. f3\n");
+        printf("4. f4\n");
+        printf("5. f5\n");
+        printf("6. f6\n");
+        printf("7. f7\n");
+		printf("\nEnter choice\n");
+		scanf("%d", &value);
+
+        if (value != 0) {
+            long a;
+            printf("Please enter value for n\n");
+            scanf("%d", &a);
+
+            clock_t start = clock();
+		}
+	}
+}
 
 int main() {
 	test_list();
